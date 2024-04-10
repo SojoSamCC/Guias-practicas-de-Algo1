754d25d89354d5695957c6f0097f44528b88b9a6
Samuel Sojo <163915138+SojoSamCC@users.noreply.github.com>
2024-04-10 12:48:08 -0300

    Terminé el segundo laboratorio de Haskell.

--- Guia_1.hs
+++ Guia_1.hs
@@ -77,10 +77,78 @@ digitoDecenas :: Int -> Int
 digitoDecenas x = div ((mod x 100)- (mod x 10)) 10 -- otra forma es x= div(mod x 100) 10
 
 -- Ejercicio 4)
--- b)
+
+-- b) todoMenor: dadas dos tuplas R × R, decide si es cierto que cada coordenada de la primera tupla es menor a la coordenada correspondiente de la segunda tupla.
+
 -- problema todoMenor(t1,t2:RXR):Bool{
 -- asegura {la primera coordenada de t1 es menor que la primera coordenada de t2 ∧ la segunda coordeanda de t1 es menor que la segunda coordenada de t2}
 --}
+
 todoMenor :: (Int,Int) -> (Int,Int) -> Bool
-todoMenor (x,y) (z,t) | x<z && y<t = True
+todoMenor (x,y) (z,t) | fst (x,y)< fst (z,t) && snd(x,y)<snd(z,t) = True
                       | otherwise =False
+
+-- f) dada una terna de enteros, devuelve la posición del primer número par si es que hay alguno, y devuelve 4 si son todos impares).
+
+-- problema posPrimerPar (t:ZxZxZ):Z {
+-- asegura {res es igual a la posicion del primer numero par de la terna t}
+-- asegura {res=1 <-> resto de x entre dos = 0}
+-- asegura {res=2 <-> resto de x entre dos = 0}
+-- asegura {res=3 <-> resto de x entre dos = 0}
+--}
+
+posPrimerPar :: (Int,Int,Int) -> Int
+posPrimerPar (x,y,z) | mod x 2 ==0 = 1
+                     | mod y 2 ==0 = 2
+                     | mod z 2 ==0 = 3
+                     | otherwise = 4
+
+-- Ejercicio 6) Programar una función bisiesto :: Integer -> Bool según la siguiente especificación:
+-- problema bisiesto (a˜no: Z) : Bool {
+-- requiere: {True}
+-- asegura: {res=false ↔ a˜no no es m´ultiplo de 4 o a˜no es
+-- múltiplo de 100 pero no de 400}
+-- }
+
+bisiesto :: Integer -> Bool
+bisiesto x | mod x 4 /=0 = False
+           | (mod x 100 ==0 && mod x 400 /=0) = False 
+           | otherwise = True
+
+
+-- Ejercicio 7) Implementar una función: distanciaManhattan:: (Float, Float, Float) -> (Float, Float, Float) -> Float
+-- problema distanciaManhattan (P: R × R × R, Q: R × R × R) :R {
+-- requiere: {True}
+-- asegura: {res = sumatoria desde i=0 a 2 de |Pi-Qi|}
+--}
+
+distanciaManhattan:: (Float, Float, Float) -> (Float,Float, Float) -> Float
+distanciaManhattan (x,y,z) (q,r,t) = abs(x-q)+abs(y-r)+abs(z-t)
+
+-- Ejercicio 8) Implementar una función comparar :: Integer -> Integer -> Integer
+-- problema comparar (a:Z, b:Z) : Z {
+-- requiere: {True}
+-- asegura: {(res= 1 ↔ sumaUltimosDosDigitos(a) <
+-- sumaUltimosDosDigitos(b))}
+-- asegura: {(res=-1 ↔ sumaUltimosDosDigitos(a) >
+-- sumaUltimosDosDigitos(b))}
+-- asegura: {(res= 0 ↔ sumaUltimosDosDigitos(a) =
+-- sumaUltimosDosDigitos(b))}
+-- }
+
+-- Conviene definir primero una función sumaUltimosDosDigitos(a)
+
+sumaUltimosDosDigitos :: Int -> Int
+sumaUltimosDosDigitos k = (digitoDecenas k) + (digitoUnidades k)
+
+comparar :: Int -> Int -> Int
+comparar x y | ((sumaUltimosDosDigitos x) < (sumaUltimosDosDigitos y)) = 1
+             | ((sumaUltimosDosDigitos x) > (sumaUltimosDosDigitos y)) = -1
+             | ((sumaUltimosDosDigitos x) == (sumaUltimosDosDigitos y)) = 0
+
+-- Ejercicio 9) 
+-- d) Esta función toma dos números reales y calcula el promedio entre ellos.
+-- e) Esta función toma una tupla de números reales y devuelve el promedio entre la primera coordenada de la tupla y la segunda coordeanda de la tupla.
+
+
+
