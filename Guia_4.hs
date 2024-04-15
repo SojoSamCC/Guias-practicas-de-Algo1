e9aef81233225aa3e1490a59ab1b990d0f428c06
Samuel Sojo <163915138+SojoSamCC@users.noreply.github.com>
2024-04-15 14:00:23 -0300

    Tengo que resolver bien dos ejercicios, revisar comentarios

--- Guia_4.hs
+++ Guia_4.hs
@@ -1,6 +1,6 @@
 factorial :: Int -> Int
 factorial n | n==0 =1
-            | n>0 = n*factorial (n-1)
+            | (abs n)>0 = (abs n)*factorial (n-1)
 
 {--
 Un comentario de varias  líneas, osi osi
@@ -25,7 +25,7 @@ fibonacci n | n==0 =0
 Ejercicio 2)
 Implementar una funci´on parteEntera :: Float -> Integer
 que calcule la parte entera de un n´umero real.
---}
+Debo de arreglar lo de resutado <= x < resultado+1 ; hacer ejemplo con un nùmero negativo --}
 parteEntera :: Float -> Integer
 parteEntera n | n >=0 &&   n <1 = 0
               | n>(-1) && n <0 = 0
@@ -37,17 +37,39 @@ Ejercicio 3)
 Especificar e implementar la funci´on esDivisible :: Integer ->Integer ->Bool que dados dos n´umeros
 naturales determinar si el primero es divisible por el segundo. No est´a permitido utilizar las funciones mod ni div.
 --}
-tieneDecimal :: Float -> Bool
-tieneDecimal n | n>1 && n>0 = tieneDecimal (n-1)
-               | n<1 && n>0 = True
-               | n==0 || n==1 = False
-{--
-esPrimo :: Integer -> Bool 
-esPrimo x | x==1 || x== 2 || x==3 || x  
---}
-
 esDivisible :: Integer -> Integer -> Bool
 esDivisible x y | y>x = False
                 | x==y = True
                 | otherwise = esDivisible (x-y) y
 
+tieneDecimal :: Float -> Bool
+tieneDecimal n | n>1 && n>0 = tieneDecimal (n-1)
+               | n<1 && n>0 = True
+               | n==0 || n==1 = False
+
+sumaImpares :: Integer -> Integer 
+sumaImpares 1 = 1
+sumaImpares n | (mod n 2) /= 0 = n + sumaImpares(n-2)
+              | otherwise = sumaImpares (n-1)
+
+medioFact :: Integer ->Integer
+medioFact n | n==0 =1 
+            | n < 0 = 1
+            | n > 0 = n* medioFact(n-2)
+
+sumaDigitos :: Integer ->Integer
+sumaDigitos n = (mod n 10) + sumaDigitos (div n 10)
+
+sacarUnidades :: Integer -> Integer
+sacarUnidades n = div n 10
+
+digitoUnidades :: Integer -> Integer
+digitoUnidades n = mod n 10
+
+todosDigitosIguales :: Integer -> Bool
+todosDigitosIguales n | n >=0 && n<10 = True 
+                      | otherwise = ((digitoUnidades n == sacarUnidades n)) -- Tengo que resolver esto, muy muy rápido
+
+{--
+iesimoDigito :: Integer ->Integer ->Integer
+iesimoDigito n | --}
