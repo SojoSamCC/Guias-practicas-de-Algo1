2c15509068440a86b477629d577a83751e80cb13
Samuel Sojo <163915138+SojoSamCC@users.noreply.github.com>
2024-05-02 17:24:26 -0300

    Terminé la guía 5.

--- guia5.hs
+++ guia5.hs
@@ -1,17 +1,18 @@
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
 {-# HLINT ignore "Use camelCase" #-}
 {-# HLINT ignore "Use list literal" #-}
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
 
 --Ejercicio 1)
 -- 1)
 longitud :: (Eq t)=>[t] -> Integer
-longitud (x:xs) | xs==[] = 1
+longitud (x:xs) | null xs = 1
                 | otherwise = 1+ longitud xs
 
 -- 2)
 ultimo :: (Eq t)=> [t] -> t
-ultimo (x:xs) | xs==[] = x
-              | otherwise = ultimo xs 
+ultimo (x:xs) | null xs = x
+              | otherwise = ultimo xs
 -- 3) 
 principio :: [t] -> [t]
 principio [x,y] = [x]
@@ -20,9 +21,9 @@ principio (x:xs) = x:principio xs
 --4)
 ultimo2 :: [t] -> t
 ultimo2 [x] = x
-ultimo2 (x:xs) = ultimo2 xs 
-              
-reverso :: [t] -> [t] 
+ultimo2 (x:xs) = ultimo2 xs
+
+reverso :: [t] -> [t]
 reverso [x,y] = [y,x]
 reverso (x:xs) = ultimo2 xs:reverso (principio (x:xs))
 
@@ -40,12 +41,12 @@ pertenece e (x:xs) = e==x || pertenece e xs
 --2)
 todosIguales :: (Eq t) => [t] -> Bool
 todosIguales [x] = True
-todosIguales (x:xs) = x ==(head xs) && todosIguales xs
+todosIguales (x:xs) = x ==head xs && todosIguales xs
 
 --3)
 todosDistintos :: (Eq t) => [t] -> Bool
 todosDistintos [x,y] = x/=y
-todosDistintos (x:xs) = not (pertenece x xs) && todosDistintos xs  
+todosDistintos (x:xs) = not (pertenece x xs) && todosDistintos xs
 
 --4)
 
@@ -71,7 +72,7 @@ quitarTodos e [x] | e==x = []
 
 quitarTodos e (x:xs) | pertenece e (x:xs) && e==x = quitarTodos e (quitar e (x:xs))
                      | pertenece e (x:xs) && e/=x = x: quitarTodos e xs
-                     | otherwise = (x:xs)
+                     | otherwise = x:xs
 
 --7)
 eliminarRepetidos :: (Eq t) => [t] -> [t]
@@ -83,7 +84,7 @@ eliminarRepetidos (x:xs) | pertenece x xs = x:eliminarRepetidos (quitarTodos x x
 --8)
 mismosElementos :: (Eq t) => [t] -> [t] -> Bool
 mismosElementos [x] [y] = x==y
-mismosElementos (x:xs) (y:ys) = longitud (x:xs)==longitud(y:ys) && pertenece x (y:ys) && pertenece y (x:xs) && mismosElementos xs ys 
+mismosElementos (x:xs) (y:ys) = longitud (x:xs)==longitud(y:ys) && pertenece x (y:ys) && pertenece y (x:xs) && mismosElementos xs ys
 
 -- 9)
 -- [1,2,3,4,5,6,7,8,5,4,3,23,2,4,5,7,78,0]
@@ -111,7 +112,7 @@ maximo (x:xs) = sacarMenor x (x:xs)
 --4) 
 sumarN :: Integer -> [Integer] -> [Integer]
 sumarN e [x] = [e+x]
-sumarN e (x:xs) = [e+x]++sumarN e xs
+sumarN e (x:xs) = (e+x) : sumarN e xs
 
 --5)
 sumarElPrimero :: [Integer] -> [Integer]
@@ -125,9 +126,9 @@ sumarElUltimo (x:xs) = sumarN (ultimo (x:xs)) (x:xs)
 
 --7)
 pares :: [Integer] -> [Integer]
-pares [x] | mod x 2 == 0 = [x]
+pares [x] | even x = [x]
           | otherwise = []
-pares (x:xs) | mod x 2 == 0 = x:pares xs
+pares (x:xs) | even x = x:pares xs
              | otherwise = pares xs
 
 --8)
@@ -136,7 +137,7 @@ multiplosDeN e [x] | mod x e ==0 = [x]
                    | otherwise = []
 multiplosDeN e (x:xs) | mod x e == 0 = x:multiplosDeN e xs
                       | otherwise = multiplosDeN e xs
-                         
+
 --9)
 menor :: (Ord s)=>[s] ->s
 menor [x] = x
@@ -162,28 +163,31 @@ ordenar2 (x:xs) = menor2 (x:xs): ordenar2 (quitar (menor2 (x:xs)) (x:xs)) -- Tam
 
 -- Ejercicio 4)
 -- a)
-sacarBlancosRepetidos :: [Char] -> [Char] -- NOTA: para referirse a los Char hay que usar ' ' y no " "
+sacarBlancosRepetidos :: String -> String -- NOTA: para referirse a los Char hay que usar ' ' y no " "
 sacarBlancosRepetidos [x,y] | x== ' ' && y== ' ' = [x]
                             | otherwise = [x,y]
 sacarBlancosRepetidos (x:xs) | x == ' ' && head xs == ' ' = x:tail xs
-                             | otherwise = x:(head xs):sacarBlancosRepetidos (tail xs)
+                             | otherwise = x:head xs:sacarBlancosRepetidos (tail xs)
 -- ['s','a','l','i','r', ' ' , ' ', 'd', 'e', ' ', 'a', 'q', ' ', ' ', 'u', 'i']
 
 -- b)
-contarPalabras :: [Char] -> Integer
-contarPalabras [x,y] | x/= ' ' && y /= ' ' = 1
-                     | otherwise = 0
-contarPalabras (x:xs) | x/= ' ' && head xs /= ' ' = 1+contarPalabras (tail xs)
-                      | otherwise = contarPalabras (tail xs)
+contarPalabras :: String -> Integer
+contarPalabras [x] | x==' ' = 0
+                   | otherwise = 1
+contarPalabras [x,y] | x==' ' && y== ' ' =0
+                     | otherwise = 1
+contarPalabras (x:y:xs) | x==' ' =0+contarPalabras (y:xs)
+                        | x/= ' ' && y==' ' = 1+contarPalabras xs
+                        | otherwise = 0+contarPalabras xs
 
 -- c)
-palabra :: [Char] -> [Char] --Esto reconoce la primera palabra en la lista
+palabra :: String -> String --Esto reconoce la primera palabra en la lista
 palabra [] = []
 palabra [' '] = []
 palabra (x:xs) | x/=' ' = x:palabra xs
                | otherwise = []
 
-quitarPalabra :: [Char] -> [Char] -> [Char] --Esto quita la palabra que quieras de cualquier lista de Char
+quitarPalabra :: String -> String -> String --Esto quita la palabra que quieras de cualquier lista de Char
 quitarPalabra _ [] = []
 quitarPalabra [] (y:ys) = y:ys
 quitarPalabra [x] [a] | x==a = []
@@ -192,7 +196,7 @@ quitarPalabra _ (' ':ys) = ys
 quitarPalabra (x:xs) (y:ys) | x ==y = quitarPalabra xs ys
                             | otherwise = y:quitarPalabra (x:xs) ys
 
-palabras :: [Char] -> [[Char]] --Esto me devuelve una lista donde los elementos son las palabras de la lista (sin los espacios)
+palabras :: String -> [String] --Esto me devuelve una lista donde los elementos son las palabras de la lista (sin los espacios)
 palabras [] = []
 palabras [x]  | x/= ' '= [[x]]
               | otherwise = [[]]
@@ -209,7 +213,11 @@ segundo :: [t] -> t
 segundo [x] = x
 segundo (x:xs) = head xs
 
-palabraMasLarga :: [Char] -> [Char]
+tercero :: [t] -> t
+tercero [x] = x
+tercero (x:xs) = head (tail xs)
+
+palabraMasLarga :: String -> String
 palabraMasLarga [] = []
 palabraMasLarga [x] = [x]
 palabraMasLarga (x:xs) | longitud (primero (palabras (x:xs))) >= longitud (segundo (palabras (x:xs))) = primero (palabras (x:xs))
@@ -217,7 +225,7 @@ palabraMasLarga (x:xs) | longitud (primero (palabras (x:xs))) >= longitud (segun
 
 -- [[' ','s','a','l','i','r', ' ' , ' ', 'd', 'e', ' ', 'a', 'q', ' ', ' ', 'u', 'i',' ']]
 --e) 
-aplanar :: [[Char]] -> [Char]
+aplanar :: [String] -> String
 aplanar [[]] = []
 aplanar [[x]] = [x]
 aplanar (x:xs) | xs /= [] = x++aplanar xs
@@ -225,19 +233,41 @@ aplanar (x:xs) | xs /= [] = x++aplanar xs
 -- [[' ','s','a','l','i','r'],['d','e'],['a','q','u','i']]
 
 --f)
-sacarBlancosRepetidos2 :: [[Char]] -> [[Char]] -- NOTA: para referirse a los Char hay que usar ' ' y no " "
-sacarBlancosRepetidos2 [x,y] | x== [' '] && y== [' '] = [x]
-                            | otherwise = [x,y]
-sacarBlancosRepetidos2 (x:xs) | x == [' '] && head xs == [' '] = x:tail xs
-                             | otherwise = x:(head xs):sacarBlancosRepetidos2 (tail xs)
+aplanarConBlancos :: [String] -> String
+aplanarConBlancos [x] =x
+aplanarConBlancos [x,y] = x++[' ']++y
+aplanarConBlancos (x:y:xs) | xs /= [] = x++[' ']++y++[' ']++aplanar xs
+                           | null xs = ' ':y
 
-aplanarConBlancos :: [[Char]] -> [Char]
-aplanarConBlancos [[]] = []
-aplanarConBlancos [[x]] = [x]
-aplanarConBlancos [[x],[y]] = [x]++[' ']++[y]
-aplanarConBlancos (x:xs) | xs /= [] = x++[' ']++aplanar xs
-                         | otherwise = ' ':x
+--g)
+listaNEspacios :: Integer -> String
+listaNEspacios 0 = []
+listaNEspacios n = ' ' : listaNEspacios (n-1)
 
+aplanarConNBlancos :: [String] -> Integer -> String
+aplanarConNBlancos [x] 1 =x
+aplanarConNBlancos [x,y] n = x++listaNEspacios n++y
+aplanarConNBlancos (x:y:xs) n | xs /= [] = x++listaNEspacios n++y++listaNEspacios n++aplanar xs
+                              | null xs = listaNEspacios n++y
+
+--Ejercicio 5)
+--1)
+quitarn :: (Eq t) => t -> [t] -> [t]
+quitarn _ [] = []
+quitarn e [x] = []
+quitarn e (x:xs) | e==x = xs
+                | not (pertenece e xs) = x:xs
+                | otherwise = x:quitar e xs
+sumatoriat :: (Num t) => [t] -> t
+sumatoriat [x] = x
+sumatoriat (x:xs) = x+sumatoriat xs
+
+sumaAcumuladaAux :: (Num t) => [t] -> [t] --Me tiene que devolver una lista donde cada elemento (de posición i) de esa lista sea la suma hasta la posición i de los 
+sumaAcumuladaAux [x] = [x]                -- elementos de la lista de entrada.
+sumaAcumuladaAux (x:xs) = sumatoriat (x:xs) : sumaAcumuladaAux (principio (x:xs))
+
+sumaAcumulada :: (Num t) => [t] -> [t]
+sumaAcumulada (x:xs) = reverso (sumaAcumuladaAux (x:xs))
 sacarMenor :: (Ord t)=>t -> [t] ->t
 sacarMenor e (x:xs) | e>=x && null xs = e
                     | e<=x && null xs = x
@@ -245,14 +275,47 @@ sacarMenor e (x:xs) | e>=x && null xs = e
                     | e<=x && xs/=[] = sacarMenor x xs
                     | otherwise = sacarMenor e (quitar x xs)
 
+--2)
+menorDivisorHasta :: Integer -> Integer -> Integer
+menorDivisorHasta n i | mod n i ==0 = i
+                      | otherwise = menorDivisorHasta n (i+1)
+
+menorDivisor :: Integer ->Integer
+menorDivisor 1 = 1
+menorDivisor n = menorDivisorHasta n 2
+
+esPrimo :: Integer ->Bool
+esPrimo n = menorDivisor n == n
+
+primosHastaAux :: [Integer] -> [Integer]
+primosHastaAux [2] = [2]
+primosHastaAux [n] | esPrimo n = n : primosHastaAux [n-1]
+                | otherwise = primosHastaAux [n-1]
+
+primosHasta :: [Integer] -> [Integer]
+primosHasta (x:xs) = reverso (primosHastaAux (x:xs))
+
+multiplicaPrimos :: [Integer] -> [Integer]
+multiplicaPrimos [0] = [0]
+multiplicaPrimos [x] | esPrimo x = [x]
+                     | not (esPrimo x) && mod x (head (primosHasta [x])) ==0 = head (primosHasta [x]) : multiplicaPrimos [div x (head (primosHasta [x]))]
+                     | not (esPrimo x) && mod x (head (primosHasta [x])) /=0 && mod x (segundo (primosHasta [x])) ==0 = segundo (primosHasta [x]) : multiplicaPrimos [div x (segundo (primosHasta [x]))]
+                     | otherwise = tercero (primosHasta [x]) : multiplicaPrimos [div x (tercero (primosHasta [x]))]
+
+descomponerEnPrimos :: [Integer] -> [[Integer]] 
+descomponerEnPrimos [x] = [multiplicaPrimos [x]]
+descomponerEnPrimos (x:xs) = multiplicaPrimos [x] : descomponerEnPrimos xs
+--descomponerEnPrimos me costó un montón. Sé que no está para nada optimizada pero ya fue, estoy cansado de darle tantas vueltas a esa función
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+--Funciones que quedaron de la clase práctica
+
 sacarMayor :: (Ord t)=>t -> [t] -> t
-sacarMayor e (x:xs) | e>=x && xs==[] = x
-                    | e<=x && xs==[] = e
+sacarMayor e (x:xs) | e>=x && null xs = x
+                    | e<=x && null xs = e
                     | e>=x && xs/=[] = sacarMayor x xs
                     | e<=x && xs/=[] = sacarMayor e xs
                     | otherwise = sacarMayor e (quitar e xs)
-
--- Ejercicio 3.9)
 
 cambiar_menor_a_mayor :: (Ord t)=>t -> t -> [t]
 cambiar_menor_a_mayor e x | e>=x = [x,e]
