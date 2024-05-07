b5f71f26dac26cf728d68e14a78b3da3efa5d88d
Samuel Sojo <163915138+SojoSamCC@users.noreply.github.com>
2024-05-06 21:25:02 -0300

    Otra Resolución

--- Parcial.hs
+++ Parcial.hs
@@ -1,63 +1,54 @@
-module Parcial where
------- Ejercicio 1 ------------------------------------------------------------------------------------------------------
+module SolucionT1 where
 
-hayQueCodificar :: Char -> [(Char,Char)]->Bool
-hayQueCodificar c [(x,y)] = c==x
-hayQueCodificar c ((x,y):xs) = c==x || hayQueCodificar c xs
--- 'a' [('a','b'),('s','t'),('g','e'),('a','r')]
+hayQueCodificar :: Char -> [(Char,Char)] -> Bool
+hayQueCodificar _ [] = False 
+hayQueCodificar c [(a,_)] = c == a
+hayQueCodificar c ((a,_):xs) 
+    |c /= a = hayQueCodificar c (xs)
+    |otherwise = True
 
+--------------------------------------------------------------------------------------------------
 
------- Ejercicio 2 ------------------------------------------------------------------------------------------------------
+cuantasVecesHayQueCodificar :: Char -> [Char] -> [(Char,Char)] -> Int
+cuantasVecesHayQueCodificar c frase mapeo
+    |hayQueCodificar c mapeo = buscarEnFrase c frase
+    |otherwise = 0
 
-cuantoSeRepite :: Char -> [Char] -> Int
-cuantoSeRepite c [x] | c==x = 1
-                     | otherwise = 0
-cuantoSeRepite c (x:xs) | c==x = 1+cuantoSeRepite c xs
-                        | otherwise = 0+ cuantoSeRepite c xs
+buscarEnFrase :: Char -> [Char] -> Int
+buscarEnFrase c [x]
+    |c == x = 1
+    |otherwise = 0
+buscarEnFrase c (x:xs)
+    |c == x = 1 + buscarEnFrase c xs
+    |otherwise = buscarEnFrase c (xs)
 
-cuantasVecesHayQueCodificar :: Char -> [Char] -> [(Char,Char)]->Int
-cuantasVecesHayQueCodificar c [t] [(x,y)] | not (hayQueCodificar c [(x,y)]) = 0
-                                          | otherwise = 1
-
-cuantasVecesHayQueCodificar c (t:xs) ((x,y):ys) | not (hayQueCodificar c ((x,y):ys)) = 0
-                                                | otherwise = cuantoSeRepite c (t:xs)
--- ['a','a','a'] [('a','b'),('s','t'),('g','e'),('a','r')]
-
------- Ejercicio 3 ------------------------------------------------------------------------------------------------------
-
-esElmismo :: Char -> [Char] -> Bool
-esElmismo c [x] = c==x
-esElmismo c (x:xs) = c==x && esElmismo c xs 
-
-quitarTodos :: Char -> [Char] -> [Char]
-quitarTodos c [x] | c==x =[]
-                  | otherwise = [x]
-quitarTodos c (x:xs) | c==x = quitarTodos c xs    
-                     | otherwise = x:quitarTodos c xs
-
-sacarRepetidos :: [Char] -> [Char]
-sacarRepetidos [] = []
-sacarRepetidos (x:xs) = x:sacarRepetidos (quitarTodos x (x:xs))
-
-segundo :: [Char] -> Char
-segundo [x] = x
-segundo (x:xs) = head xs
+--------------------------------------------------------------------------------------------------
 
 laQueMasHayQueCodificar :: [Char] -> [(Char,Char)] -> Char
-laQueMasHayQueCodificar (x:xs) ((a,b):as) | esElmismo x (x:xs) = x
-                                          | (cuantasVecesHayQueCodificar (head (sacarRepetidos (x:xs))) (x:xs) ((a,b):as)) >= (cuantasVecesHayQueCodificar (segundo (sacarRepetidos (x:xs))) (x:xs) ((a,b):as)) = laQueMasHayQueCodificar (quitarTodos (segundo (sacarRepetidos (x:xs))) (x:xs)) ((a,b):as)
-                                          | otherwise = laQueMasHayQueCodificar (quitarTodos (head (sacarRepetidos (x:xs))) (x:xs)) ((a,b):as)
+laQueMasHayQueCodificar [x] mapeo = x -- no veo si hayQueCodificar x == True porque aseguran que uno pertenece si o si
+laQueMasHayQueCodificar (x:xs) mapeo
+    |cuantasVecesHayQueCodificar x (x:xs) mapeo == maximo (x:xs) mapeo = x
+    |otherwise = laQueMasHayQueCodificar (xs) mapeo
 
------- Ejercicio 4 ------------------------------------------------------------------------------------------------------
+maximo :: [Char] -> [(Char,Char)] -> Int
+maximo [x] _ = 1
+maximo (x:xs) mapeo
+    |cuantasVecesHayQueCodificar x (x:xs) mapeo > maximo (xs) mapeo = cuantasVecesHayQueCodificar x (x:xs) mapeo
+    |otherwise = maximo (xs) mapeo
 
-codigo :: Char -> [(Char,Char)] -> Char
-codigo c ((a,b):as) | c==a = b
-                    | otherwise = codigo c as
+--------------------------------------------------------------------------------------------------
+
 codificarFrase :: [Char] -> [(Char,Char)] -> [Char]
-codificarFrase [x] ((a,b):as) | hayQueCodificar x ((a,b):as) = [codigo x ((a,b):as)] 
-                              | otherwise = [x]
-codificarFrase (x:xs) ((a,b):as) | hayQueCodificar x ((a,b):as) = codigo x ((a,b):as):codificarFrase xs ((a,b):as)
-                                 | otherwise = x:codificarFrase xs ((a,b):as)
+codificarFrase [x] mapeo
+    |hayQueCodificar x mapeo = [buscarCodificado x mapeo]
+    |otherwise = [x]
+codificarFrase (x:xs) mapeo
+    |hayQueCodificar x mapeo = buscarCodificado x mapeo : codificarFrase (xs) mapeo 
+    |otherwise = x : codificarFrase (xs) mapeo
 
--- ['s','o','s'] [('s','o'),('o','t')]
--- ['s','o','s','a'] [('s','o'),('o','t')]
+buscarCodificado :: Char -> [(Char, Char)] -> Char
+buscarCodificado c ((a,b):xs)
+    |c == a = b
+    |otherwise = buscarCodificado c xs
+
+--------------------------------------------------------------------------------------------------
